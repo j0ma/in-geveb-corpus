43ce7c39e2146d3505a94943fee4fa1a7b5a6113
Jonne Saleva <jonne@jonnesaleva.com>
2022-05-31 09:53:27 -0700

    Pretty printing using printf

--- src/scrape_all_articles.sh
+++ src/scrape_all_articles.sh
@@ -1,13 +1,39 @@
 OUTPUT_FOLDER=${1:-../corpus}
 ARTICLE_LIST=$(cat ../data/article_links.txt)
-COUNTER=$(ls ${OUTPUT_FOLDER}/*.txt | cut -f1 -d'-' | sort -nr | head -n 1)
+
+# Make output dir if needed
+mkdir -pv "${OUTPUT_FOLDER}"
+
+# Get last article id 
+LAST_ARTICLE_ID=$(
+    ls ${OUTPUT_FOLDER}/*.txt \
+        | while read line; do basename $line; done \
+        | cut -f1 -d'-' \
+        | sort -nr \
+        | head -n 1
+)
+
+echo "Checking whether there are existing articles in ${OUTPUT_FOLDER}."
+if [ -n "${LAST_ARTICLE_ID}" ]
+then
+    COUNTER=$((LAST_ARTICLE_ID+1))
+    echo "Last article id: ${LAST_ARTICLE_ID}. Starting at ${COUNTER}"
+else
+    COUNTER=1
+    echo "No existing articles found in ${OUTPUT_FOLDER}. Starting at ${COUNTER}."
+fi
+
+# Selector to search for
 CSS="section#main-content > div.block-translatedText > div.translation-wrapper  > div.yiddish-text > p"
+
 for a in $ARTICLE_LIST
 do
+    echo # new line
 
     # parse article name from URL
     URL=$a
     ARTICLE_NAME=$(echo $a | sed s/"https:\/\/ingeveb.org\/texts-and-translations\/"//g)
+    echo "Article: $ARTICLE_NAME"
 
     # check if article already exists
     FOUND_ARTICLE=$(ls ${OUTPUT_FOLDER} | rg ${ARTICLE_NAME})
@@ -17,22 +43,22 @@ do
         OUTPUT="${OUTPUT_FOLDER}/$COUNTER-$ARTICLE_NAME.txt"
         
         # make actual python script call
-        echo "Beginning article scrape for article: $ARTICLE_NAME"
-        #python scrape_individual_article.py \
-               #--url="$URL" \
-               #--css="$CSS" \
-               #--output="$OUTPUT" \
-               #--format="text"
-        sleep 1
-        echo "Article scrape done. Incrementing counter..."
+        printf "\tBeginning scrape. Output id: ${COUNTER}\n"
+        printf "\tOutput file:: ${OUTPUT}\n"
+        python scrape_individual_article.py \
+               --url="$URL" \
+               --css="$CSS" \
+               --output="$OUTPUT" \
+               --format="text"
+        printf "\tArticle scrape done. Incrementing counter...\n"
         echo
 
         # increment counter at end of loop
         COUNTER=$((COUNTER+1))
 
     else
-        echo "Article ${ARTICLE_NAME} found under ${FOUND_ARTICLE}"
-        echo "Moving on..."
+        printf "\tArticle found under ${FOUND_ARTICLE}\n"
+        printf "\tMoving on...\n"
     fi
 
 done
